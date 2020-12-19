f6f245a4f9ce915c26ca51e49d0eed11d6fbe968
Kevin Scaringi <kevinscaringi@Kevins-MBP.attlocal.net>
2020-12-19 00:41:32 -0800

    minor tweaks for navbar

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask_login import login_required, current_user
-from flask import render_template
+from flask import render_template, session
 from models import *
 from config import *
 from routes.login import *
@@ -29,39 +29,16 @@ from functions import *
 # Home Page + Profile Page
 @app.route('/')
 def index():
+    if current_user.is_authenticated:
+        session['name'] = current_user.name
+    else:
+        session['name'] = ''
     return render_template('index.html')
 
 @app.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
-
-
-
-
-'''
- _____
-< API >
- -----
-   \
-    \
-     \
-        __ \ / __
-       /  \ | /  \
-           \|/
-       _.---v---.,_
-      /            \  /\__/\
-     /              \ \_  _/
-     |__ @           |_/ /
-      _/                /
-      \       \__,     /
-   ~~~~\~~~~~~~~~~~~~~`~~~
-'''
-@app.route('/api')
-def api_home():
-    return {"Hello" : "World"}
-
-# Removed API Logic on 11/28/2020 @ 9:52pm ET
 
 if __name__ == '__main__':
     app.run(debug=True)
